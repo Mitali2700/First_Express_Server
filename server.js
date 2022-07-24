fad3ec9040f3f9cf54cc9af92742bac467c79ee6
Mitali2700 <201B155@juetguna.in>
2022-07-24 23:01:14 +0530

    Get Request

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 //jshint esversion:6
 const express = require("express");
 const app = express();
+
+app.get("/", function(request,response){
+    console.log(request);
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
@@ -9,3 +14,11 @@ app.listen(3000, function(){
 /* Now you can see that they've got this command ‘npm install express - - save’. Now, as they explain
 down here, what ‘ - - save’ basically does is it adds Express into your packages.json under the
 dependencies.*/
+
+/*
+Well, it means that when our browser is trying to get in touch with our server on the port 3000, it's
+not able to get anything back.
+Now we have to figure out how can we write some code so that our server responds when a browser is making
+a request to our server.
+We have to send the browser some information, right, to display.
+*/
