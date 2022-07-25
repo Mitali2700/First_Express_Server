862facc9d72e3b4c8643d1532ba5b5e01401957b
Mitali2700 <201B155@juetguna.in>
2022-07-25 15:19:21 +0530

    ading contact to our server

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get("/", function(request,response){
     response.send("<h1>Hello World!!</h1>");
 });
 
+app.get("/contact", function(request,response){
+    //console.log(request);
+    response.send("Contact me at: mitalicwa@gmail.com");
+});
+
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
